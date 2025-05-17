90d1316f654e9d5c914fd49616785935251e71d3
codebyhiranmai <hiranmaikuruvella@gmail.com>
2025-05-17 17:29:12 +0530

    Initial commit

---
+++ game.js
@@ -0,0 +1,95 @@
+let boxes=document.querySelectorAll(".box");
+let resetBtn=document.querySelector("#reset-btn");
+let newGameBtn=document.querySelector("#newgame-btn");
+let msgContainer=document.querySelector(".msg-container")
+let msg=document.querySelector("#msg");
+
+let turnO=true; //playerX,playerO
+ 
+const winPattern=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+
+const resetGame=()=>{
+    turnO=true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if(turnO){
+            box.innerText="O";
+            box.style.color = "#98CE00";
+            turnO = false ;
+        }else{
+            box.innerText="X";
+            turnO = true;
+            box.style.color = "#DBD3D8";
+        };
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+
+const disableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+};
+const enableBoxes=()=>{
+    for (let box of boxes){
+        box.disabled= false;
+        box.innerText="";
+    }
+};
+
+const showWinner=(winner)=>{
+    msg.innerText=`Congratulations,Winner is ${winner}`;
+    msgContainer.classList.remove("hide"); 
+    disableBoxes();
+};
+
+let checkWinner=()=>{
+    for(let pattern of winPattern){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val !=""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                showWinner(pos1Val);
+            };
+        };
+    };
+};
+
+newGameBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
+
+// let draw=()=>{
+//     for(let pattern of winPattern){
+//         let pos1Val = boxes[pattern[0]].innerText;
+//         let pos2Val = boxes[pattern[1]].innerText;
+//         let pos3Val = boxes[pattern[2]].innerText;
+
+//         if(pos1Val != "" || pos2Val != "" || pos3Val !=""){
+//                 console.log("Its a draw");
+//         };
+//     }
+// }
+// const drawMatch=(draw)=>{
+//     msg.innerText=`It's a draw match`;
+//     msgContainer.classList.remove("hide"); 
+//     disableBoxes();
+// };
+
+
